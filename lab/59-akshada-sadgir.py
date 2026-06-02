5402f35ecdf334a2290f8d29f5d75a984e6c9c56
akshadasadgir87-png <akshadasadgir87@gmail.com>
2026-06-02 11:32:48 +0530

    Update 59-akshada-sadgir.py

--- lab/59-akshada-sadgir.py
+++ lab/59-akshada-sadgir.py
@@ -4,4 +4,15 @@ while i<=10
   sum=sum+i
   i=i+1
 print(sum)
-  
+#problem2
+a=4
+b=9
+c=19
+def numbers(n):
+    if n%2==0:
+        print("even")
+    else:
+        print("odd")
+numbers(a)
+numbers(b)
+numbers(c)
