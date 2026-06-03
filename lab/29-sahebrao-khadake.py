22c167dc7a795204b8db7a607205bad6cb49a34d
sahebraokhadake812-svg <sahebraokhadake812@gmail.com>
2026-06-03 11:35:48 +0530

    Create 29-sahebrao-khadake.py

---
+++ lab/29-sahebrao-khadake.py
@@ -0,0 +1,19 @@
+#problem 5
+l=[1,2,3,4,5,6]
+l.pop(0)
+print(l)
+l.pop(1)
+print(l)
+l.pop(2)
+print(l)
+
+#problem 6
+l=[1,2,3,4,5,6]
+l.pop(4)
+print (l)
+l.pop(2)
+print (l)
+l.pop(0)
+print (l)
+
+#problem 
