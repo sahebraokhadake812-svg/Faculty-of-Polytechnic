5084c23507ffa49f0b1df70d9921b03b16d041f1
pc307362-ctrl <pc307362@gmail.com>
2026-06-05 12:03:38 +0530

    Update solution3.py

--- students/pooja-chaudhari/solution3.py
+++ students/pooja-chaudhari/solution3.py
@@ -5,3 +5,16 @@ for x in s:
     if x not in['a','e','i','o','u']:
         count=count+1
 print("no of constonants=",count)
+#problem 2
+s={'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l','m', 'n','o' , 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'}
+v=set()
+c=set()
+for x in s: 
+    if x in ['a','e','i','o','u']:
+        v.add(x)
+    else:
+        c.add(x)
+print("vowels=",v)
+print("consonants=",c)
+        
+
